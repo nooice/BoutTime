dd6b0c45cb5450e51bdbe0d3e001604ba7c69691
nooice <imagetskanky@macbook-pro-2.local>
2017-05-01 21:06:39 -0600

    First upload

---
+++ BoutTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BoutTime
+//
+//  Created by Kalvin Bunn on 5/1/17.
+//  Copyright © 2017 Kalvin Bunn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
